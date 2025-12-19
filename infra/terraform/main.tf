25318d7cbe3b240675da4917a8c60c764d267e00
Thumati Praveen Kumar <tumatipraveenreddy23@gmail.com>
2025-12-19 12:36:02 -0600

    Add Container Apps Environment wired to Log Analytics

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -5,3 +5,18 @@ locals {
 data "azurerm_resource_group" "rg" {
   name = var.resource_group_name
 }
+
+resource "azurerm_log_analytics_workspace" "law" {
+  name                = "${local.prefix}-law"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+resource "azurerm_container_app_environment" "cae" {
+  name                       = "${local.prefix}-cae"
+  location                   = data.azurerm_resource_group.rg.location
+  resource_group_name        = data.azurerm_resource_group.rg.name
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.law.id
+}
--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -5,3 +5,12 @@ output "resource_group_name" {
 output "location" {
   value = data.azurerm_resource_group.rg.location
 }
+
+output "log_analytics_workspace_id" {
+  value = azurerm_log_analytics_workspace.law.id
+}
+
+
+output "container_app_environment_id" {
+  value = azurerm_container_app_environment.cae.id
+}
